5e1eed7c1855c76b81bf30a6b9bcfa71af15812d
Günışığı Zan Diemer <43114601+gzandiemer@users.noreply.github.com>
2020-09-26 12:38:26 +0200

    add semicolons

--- algos/level2/week1-130920/gzan.index.js
+++ algos/level2/week1-130920/gzan.index.js
@@ -1,9 +1,9 @@
 function potatoes(waterP0, totalWeight0, waterP1) {
     // P0 for initial percentage, P1 for final percentage
-    const dryMatterP0 = 100 - waterP0
-    const dryMatterP1 = 100 - waterP1
-    const totalWeight1 = totalWeight0 * dryMatterP0 / dryMatterP1
-    return Math.trunc(totalWeight1)
+    const dryMatterP0 = 100 - waterP0;
+    const dryMatterP1 = 100 - waterP1;
+    const totalWeight1 = totalWeight0 * dryMatterP0 / dryMatterP1;
+    return Math.trunc(totalWeight1);
 }
 
 module.exports = potatoes;
