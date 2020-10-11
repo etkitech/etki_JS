f1e0996e001e8cd3ff5b5df373bda7645cd2528a
gzandiemer <g.z.diemer@gmail.com>
2020-10-11 16:39:45 +0200

    refactor solution l2 w3

--- algos/level2/week3/gzan.index.js
+++ algos/level2/week3/gzan.index.js
@@ -2,29 +2,14 @@ function getScore(arr) {
   let score = 0;
   let level = 0;
   let totalLines = 0;
+  let points = { "0": 0, "1": 40, "2": 100, "3": 300, "4": 1200 }
 
   for (let lines of arr) {
-    if (lines !== 0) {
-      switch (lines) {
-        case 1:
-          points = 40;
-          break;
-        case 2:
-          points = 100;
-          break;
-        case 3:
-          points = 300;
-          break;
-        case 4:
-          points = 1200;
-          break;
-      }
-      score += points * (level + 1)
-      totalLines += lines
-      if (totalLines >= 10) {
-        level++
-        totalLines -= 10
-      }
+    score += points[lines] * (level + 1)
+    totalLines += lines
+    if (totalLines >= 10) {
+      level++
+      totalLines -= 10
     }
   }
   return score;
