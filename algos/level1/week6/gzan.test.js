797dc525fc76468e265f861879e7540ebd0cf951
gzandiemer <g.z.diemer@gmail.com>
2020-11-01 16:07:47 +0100

    edit test names

--- algos/level1/week6/gzan.test.js
+++ algos/level1/week6/gzan.test.js
@@ -4,18 +4,18 @@ test('generateRange', () => {
   expect(generateRange).toBeDefined();
 });
 
-test('generateRanges an arr with mixed integers', () => {
+test('test 1', () => {
   expect(generateRange(2, 10, 2)).toEqual([2, 4, 6, 8, 10]);
 });
 
-test('generateRanges an arr with mixed integers', () => {
+test('test 2', () => {
   expect(generateRange(1, 10, 5)).toEqual([1, 6]);
 });
 
-test('generateRanges an arr with mixed integers', () => {
+test('test 3', () => {
   expect(generateRange(11, 44, 8)).toEqual([11, 19, 27, 35, 43]);
 });
 
-test('generateRanges an arr with mixed integers', () => {
+test('test 4', () => {
   expect(generateRange(12, 130, 4)).toEqual([12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 124, 128]);
 });
