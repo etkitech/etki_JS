ed9d15c56b9bec5c1c7b780365329d0427248979
gzandiemer <g.z.diemer@gmail.com>
2020-11-29 14:04:19 +0100

    add solutions week10

--- algos/level1/week10/gzan.test.js
+++ algos/level1/week10/gzan.test.js
@@ -9,7 +9,7 @@ describe('Type', function () {
 });
 
 describe('Logic', function () {
-  it('should find the correct killer', () => {
+  it('should find the length of shortest word', () => {
     expect(findShortest("What is now proved was once only imagined")).toEqual(2);
     expect(findShortest("We are all in the gutter but some of us are looking at the stars")).toEqual(2);
     expect(findShortest("Lorem ipsum dolor consectetur adipiscing finibus metus varius")).toEqual(5);
@@ -17,7 +17,7 @@ describe('Logic', function () {
 });
 
 describe('Logic', function () {
-  it('should find the killer', () => {
+  it('should find the length of shortest word', () => {
     expect(findShortestWordLength("Neque porro quisquam dolorem ipsum quia consectetur adipisci")).toEqual(4);
     expect(findShortestWordLength("consectetur adipiscing Quisque lacinia molestie gravida eleifend mauris libero dapibussagittis sagittis Maecenas")).toEqual(6);
     expect(findShortestWordLength("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo Nemo enim ipsam voluptatem r")).toEqual(1);
