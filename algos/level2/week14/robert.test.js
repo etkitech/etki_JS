97d77311bcac5ddaf7cbecf3765a88712d0efb59
robertito1 <orazurobert@yahoo.com>
2021-01-08 20:32:04 +0000

    algos week14

--- algos/level2/week14/robert.test.js
+++ algos/level2/week14/robert.test.js
@@ -1,6 +1,6 @@
 const Queue = require('./robert.index')
 
-    describe('Test that class is imported correctly',()=>{
+    describe('Test that class is imported/exported correctly',()=>{
         test('Queue should be defined', () => {
             expect(Queue).toBeDefined();
         });
