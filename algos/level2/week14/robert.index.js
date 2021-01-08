c62768cb1206a099805f5b853dd431e69c8a6c41
robertito1 <orazurobert@yahoo.com>
2021-01-08 20:16:54 +0000

    algos week14

---
+++ algos/level2/week14/robert.index.js
@@ -0,0 +1,28 @@
+class Queue {
+    constructor(arrayOfValues) {
+     this.values = arrayOfValues ? arrayOfValues : []
+    }
+    add(value) {
+        return this.values = [value, ...this.values]
+    }
+     
+    remove() {
+    const valueToBeDeleted = this.oldest()
+        this.values = this.values.slice(0, this.values.length - 1);
+        return valueToBeDeleted
+    }
+     
+    oldest(){
+     return this.values[this.values.length - 1]
+    }
+
+    newest(){
+      return this.values[0]
+    }
+
+    size(){
+        return this.values.length
+    }
+  }
+// const my = new Queue([1,3])
+module.exports = Queue;
