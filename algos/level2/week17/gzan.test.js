77a1ab599a6345c18b3292210599213379d94fe6
gzandiemer <g.z.diemer@gmail.com>
2021-01-31 18:51:09 +0100

    add l2 w17 solutions

---
+++ algos/level2/week17/gzan.test.js
@@ -0,0 +1,36 @@
+const Queue = require('./gzan.index')
+
+test('Queue is a class', () => {
+  expect(typeof Queue.prototype.constructor).toEqual('function');
+});
+
+test('can add elements to a queue', () => {
+  const q = new Queue();
+  expect(() => {
+    q.add(1);
+  }).not.toThrow();
+});
+
+test('can remove elements from a queue', () => {
+  const q = new Queue();
+  expect(() => {
+    q.add(1);
+    q.remove();
+  }).not.toThrow();
+});
+
+test('Order of elements is maintained', () => {
+  const q = new Queue();
+  q.add(1);
+  q.add(2);
+  q.add(3);
+  expect(q.peek()).toEqual(1);
+  expect(q.remove()).toEqual(1);
+  expect(q.peek()).toEqual(2);
+  expect(q.remove()).toEqual(2);
+  expect(q.remove()).toEqual(3);
+  expect(q.remove()).toEqual(undefined);
+});
+
+//Source: The Coding Interview Bootcamp: Algorithms + Data Structures by Stephen Grider at Udemy
+
