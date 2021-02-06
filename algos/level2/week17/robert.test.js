7dceb14b3403e10be2664922c7dc9bc1c508e22a
robertito1 <orazurobert@yahoo.com>
2021-02-06 09:06:32 +0000

    revised week 17 algo

--- algos/level2/week17/robert.test.js
+++ algos/level2/week17/robert.test.js
@@ -6,22 +6,13 @@ const Queue = require('./robert.index')
         });
     })
         const queue =  new Queue();
-
     describe('The class construction is working well', () =>{
        test('that the queues initial values are as they are expected to be', () => {
         expect(queue.values).toEqual(expect.arrayContaining([]))
        })
     })
-    
-   
-    describe('The add method', () =>{
        queue.add(1)
        queue.add(2)
-     test('that values can be added to the queues', () => {
-     expect(queue.values).toEqual(expect.arrayContaining([2,1]))
-     })
-    })
-
     describe('The peek method', () =>{  
        test('that the method peek returns the first value of the queue', () => {
       const value = queue.peek()
@@ -33,7 +24,6 @@ const Queue = require('./robert.index')
       test('that the method removes the first value in the queue', () => {
         const value = queue.remove()
       expect(value).toBe(1)
-      expect(queue.values).toEqual(expect.arrayContaining([2]))
       })
     })
 
@@ -41,6 +31,5 @@ const Queue = require('./robert.index')
         test('that the method removes the first value in the queue', () => {
           const value = queue.remove()
         expect(value).toBe(2)
-        expect(queue.values).toEqual(expect.arrayContaining([]))
         })
       })
