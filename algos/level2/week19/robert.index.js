813cf7b0d5429eb27e668d6c2fb881b4b27a402a
robertito1 <orazurobert@yahoo.com>
2021-02-13 20:50:33 +0000

    Algo week 19

--- algos/level2/week19/robert.index.js
+++ algos/level2/week19/robert.index.js
@@ -1,6 +1,7 @@
-const {LinkedList} = require('../week18/gzan.index');
-
 let midpoint = (list) =>{
+    if(!list.head){
+    return null
+    }
 let middleNode = list.head
 let lastNode = list.head
 while(lastNode.next && lastNode.next.next){
@@ -11,12 +12,3 @@ return {data: middleNode.data}
 }
 
 module.exports = midpoint
-// const l = new LinkedList();
-//    l.insertLast('a')
-//    l.insertLast('b')
-//    l.insertLast('c')
-//    l.insertLast('d')
-//    l.insertLast('e')
-//    l.insertLast('f')
-//    l.insertLast('g')
-//    console.log(midpoint(l))
